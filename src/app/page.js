b9479f0f51038973dcadd19ba391d6979135b4ff
B-Shankar <bhimashankar@swingbell.com>
2025-05-16 22:59:40 +0530

    tailwind css

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 const Home = () => {
   return (
     <div>
-      <h1>Welcome to Bhimashankar Folio</h1>
+      <h1 className="text-3xl font-bold underline">Welcome to Bhimashankar Folio</h1>
     </div>
   );
 }
