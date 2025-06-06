17682418be2c89a8da1b57e95bfe65de081bda20
B-Shankar <bhimashankar@swingbell.com>
2025-06-06 22:46:04 +0530

    Quick Fix

--- src/app/pages/DownloadCV.jsx
+++ src/app/pages/DownloadCV.jsx
@@ -4,7 +4,7 @@ import React from "react";
 const DownloadCV = () => {
   return (
     <a
-      href="/cv/Bhimashankar Holkundi.pdf" // Path to your CV file in the public folder
+      href="/cv/Bhimashankar Holkundi-1.pdf" // Path to your CV file in the public folder
       download="Bhimashankar Holkundi.pdf" // Name of the file when downloaded
       className="flex items-center justify-center px-4 py-2 border-2 border-primary rounded-full text-primary font-semibold hover:text-black hover:bg-white transition duration-300"
     >
--- src/components/FloatingBar.jsx
+++ src/components/FloatingBar.jsx
@@ -15,7 +15,7 @@ const FloatingBar = () => {
     <div className="pointer-events-none fixed inset-x-0 bottom-0 md:top-4 z-30 mx-auto mb-4 flex origin-bottom h-full max-h-14 ">
       <div className="fixed bottom-0 inset-x-0 h-16 w-full bg-background to-transparent backdrop-blur-lg [-webkit-mask-image:linear-gradient(to_top,black,transparent)] dark:bg-background"></div>
       <div className="w-max p-2 rounded-full border z-50 pointer-events-auto relative mx-auto flex min-h-full h-full items-center px-1 bg-background [box-shadow:0_0_0_1px_rgba(0,0,0,.03),0_2px_4px_rgba(0,0,0,.05),0_12px_24px_rgba(0,0,0,.05)] transform-gpu dark:[border:1px_solid_rgba(255,255,255,.1)] dark:[box-shadow:0_-20px_80px_-20px_#ffffff1f_inset]">
-        <div className="flex aspect-square cursor-pointer items-center justify-center rounded-full w-[40px]">
+        {/* <div className="flex aspect-square cursor-pointer items-center justify-center rounded-full w-[40px]">
           <Link
             className="inline-flex items-center justify-center whitespace-nowrap text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground rounded-full size-12"
             aria-label="Home"
@@ -24,7 +24,7 @@ const FloatingBar = () => {
           >
             <Home />
           </Link>
-        </div>
+        </div> */}
         <div
           data-orientation="vertical"
           role="none"
@@ -71,13 +71,13 @@ const FloatingBar = () => {
             className="inline-flex items-center justify-center whitespace-nowrap text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground rounded-full size-12"
             aria-label="Youtube"
             data-state="closed"
-            href="https://t.me/raiashwith"
+            href="https://x.com/bhimashankar_"
             target="_blank"
           >
             <Twitter />
           </Link>
         </div>
-        <div
+        {/* <div
           data-orientation="vertical"
           role="none"
           className="shrink-0 bg-border w-[1px] h-full py-2"
@@ -86,8 +86,8 @@ const FloatingBar = () => {
           distance="140"
         ></div>
         <div className="flex aspect-square cursor-pointer items-center justify-center rounded-full w-[40px]">
-      <ToggleThemeBtn />
-        </div>
+          <ToggleThemeBtn />
+        </div> */}
       </div>
     </div>
   );
